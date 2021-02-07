76eb0dbad376a86a9d995a701ccb95d2b4304eb6
Victor Rudkovsky <vpcomtek@gmail.com>
2021-02-08 00:14:56 +0200

    -- init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Java Web
+
+Java Servlets
+
+
+## License
+Collision is an open-sourced software licensed under the MIT license.
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import service.Calculator;
+import servlet.CalcServlet;
+import servlet.HelloServlet;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println(1);
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        ServletContextHandler handler = new ServletContextHandler();
+        handler.addServlet(HelloServlet.class, "/");
+        handler.addServlet(new ServletHolder(new CalcServlet(new Calculator())), "/calc");
+
+        server.setHandler(handler);
+        server.start();
+        server.join();
     }
 }
